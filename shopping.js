d80112ad90cd5289050be127d47bdfaeda57c974
aar-atiq <aar.msdeveloper01@gmail.com>
2021-01-27 14:33:26 +0600

    updated current value

--- shopping.js
+++ shopping.js
@@ -1,4 +1,16 @@
-const plusBtn = document.getElementById("plusButton").addEventListener("click", function(){
-  const currentIn = document.getElementById("currentInput");
-  currentIn.value = parseFloat(currentInput.value)+1;
+const plusBtn = document.getElementById("plusButton").addEventListener("click", function () {
+    const currentIn = document.getElementById("currentInput").value;
+    const currentInM = parseFloat(currentIn) + 1;
+    document.getElementById("currentInput").value = currentInM;
+
+    const currentText = document.getElementById("currentText").innerText;
+    const currentTextM = parseFloat(currentText);
+    const totalAmount = currentInM + currentTextM;
+    document.getElementById("currentText").innerText = totalAmount;
+    
+    });
+
+const minusBtn = document.getElementById("minusButton").addEventListener("click", function () {
+    const currentIn = document.getElementById("currentInput");
+    currentIn.value = parseFloat(currentIn.value) - 1;
 })
