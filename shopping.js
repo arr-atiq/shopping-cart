3171d492f634c5e3588e12cb80313f9526ce967d
aar-atiq <aar.msdeveloper01@gmail.com>
2021-01-29 01:53:12 +0600

    organised code

--- shopping.js
+++ shopping.js
@@ -1,16 +1,40 @@
-const plusBtn = document.getElementById("plusButton").addEventListener("click", function () {
-    const currentIn = document.getElementById("currentInput").value;
-    const currentInM = parseFloat(currentIn) + 1;
-    document.getElementById("currentInput").value = currentInM;
+// document.getElementById("plusButton").addEventListener("click", function () {
+//     const currentIn = document.getElementById("currentInput");
+//     const currentInP = parseFloat(currentIn.value) + 1;
+//     currentIn.value = currentInP;
+//     const plusTotalCurrent = currentInP * 1219;
+//     document.getElementById("currentText").innerText = plusTotalCurrent;
 
-    const currentText = document.getElementById("currentText").innerText;
-    const currentTextM = parseFloat(currentText);
-    const totalAmount = currentInM + currentTextM;
-    document.getElementById("currentText").innerText = totalAmount;
-    
+//     });
+
+// document.getElementById("minusButton").addEventListener("click", function () {
+//     const currentIn = document.getElementById("currentInput");
+//     currentInM = parseFloat(currentIn.value) - 1;
+//     currentIn.value = currentInM;
+//     const minusCurrentTotal = currentInM * 1219;
+//     document.getElementById("currentText").innerText = minusCurrentTotal;
+
+// });
+
+    document.getElementById("plusButton").addEventListener("click", function () {
+        handleBtnWithPrice(true);
+    });
+    document.getElementById("minusButton").addEventListener("click", function () {
+        handleBtnWithPrice(false);
     });
 
-const minusBtn = document.getElementById("minusButton").addEventListener("click", function () {
-    const currentIn = document.getElementById("currentInput");
-    currentIn.value = parseFloat(currentIn.value) - 1;
-})
+    function handleBtnWithPrice(isIncrease){
+        const currentIn = document.getElementById("currentInput");
+        const currentInP = parseFloat(currentIn.value);
+        let newCurrentInP = currentInP;
+
+        if(isIncrease == false && newCurrentInP > 0){
+            newCurrentInP = currentInP -1;
+        }
+        if(isIncrease == true){
+            newCurrentInP = currentInP +1;
+        }
+        currentIn.value = newCurrentInP;
+        const plusTotalCurrent = newCurrentInP * 1219;
+        document.getElementById("currentText").innerText = plusTotalCurrent;
+    }
